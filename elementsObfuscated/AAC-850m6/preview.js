19263586c3801b9c6d50a799b15baff9491e6399
Андрій Пічкорський <plugins@zeroqode.com>
2023-11-16 19:22:18 +0200

    obfuscation

---
+++ elementsObfuscated/AAC-850m6/preview.js
@@ -0,0 +1,4 @@
+function(instance, properties) {
+	function preview(_0x476ebc,_0x309c2e){}
+	return preview(instance, properties);
+}
---
+++ elementsObfuscated/AAC-850m6/reset.js
@@ -0,0 +1,4 @@
+function(instance, context) {
+	function reset(_0x351126,_0x51493a){}
+	return reset(instance, context);
+}
